c3dcd848a1af4867157d7f097b33f5f0c592efb3
vominhquann <vominhquan2k2yb@gmail.com>
2023-08-11 04:44:50 +0700

    alu

---
+++ Acceptance/lmao.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+
+    public function _before(AcceptanceTester $I)
+    {
+        
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('quickstart');
+    }
+    
+}
